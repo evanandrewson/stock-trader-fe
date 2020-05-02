3ec3c599f8257d5d2ee6b1bed2a21de138acde0d
evanandrewson <evanandrewson@gmail.com>
2020-05-02 13:22:50 -0700

    added session selectors

---
+++ src/selectors/sessionSelectors.js
@@ -0,0 +1,5 @@
+export const getSession = state => state.session;
+export const getSessionId = state => getSession(state)._id;
+export const getSessionUsername = state => getSession(state).username;
+export const getSessionLoading = state => getSession(state).loading;
+export const getSessionError = state => getSession(state).error;
